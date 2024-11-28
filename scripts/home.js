e11f9ca5b91c7fb24e8d4b1f4b4fa35a0b90859b
PTParra <49121638+PTParra@users.noreply.github.com>
2024-11-28 09:33:05 +0100

    estilos seccion hotel-photos hecho

--- scripts/home.js
+++ scripts/home.js
@@ -28,6 +28,7 @@ const swiperFacilites = new Swiper('.features-list-swiper', {
 });
 
 
+
 const swiperMenu = new Swiper('.food-list-swiper', {
   // Optional parameters
   loop: true,
@@ -36,6 +37,20 @@ const swiperMenu = new Swiper('.food-list-swiper', {
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
+
+const swiperHotelPhotos = new Swiper('.hotel-images-swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  // Navigation arrows
+  pagination: {
+    el: '.swiper-pagination',
   },
   autoplay: {
     delay: 5000,
