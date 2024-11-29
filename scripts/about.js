43d352b1115b395fce909d6a8a37bfea6258340c
PTParra <49121638+PTParra@users.noreply.github.com>
2024-11-29 23:18:29 +0100

    pagina about desktop

--- scripts/about.js
+++ scripts/about.js
@@ -1,27 +1,58 @@
-const swiperFacilites = new Swiper('.facilities-list-swiper', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  // Navigation arrows
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-});
+const checkVariableSwipers = () => {
+  const anchoVentana = window.innerWidth;
 
-const swiperCounter = new Swiper('.counter__images-swiper', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-  // Navigation arrows
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-});
+  if (anchoVentana < 1000) {
+    if(!swiperFacilites){
+      swiperFacilites = new Swiper('.facilities-list-swiper', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 1,
+        // Navigation arrows
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+      });
+
+    }
+    if(!swiperCounter){
+      swiperCounter = new Swiper('.counter__images-swiper', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 1,
+        // Navigation arrows
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        autoplay: {
+          delay: 5000,
+          disableOnInteraction: false,
+        },
+      });
+    }
+
+  }else{
+
+    if(swiperFacilites){
+      swiperFacilites.destroy(true, true);
+      swiperFacilites = false;
+    }
+
+    if(swiperCounter){
+      swiperCounter.destroy(true, true);
+      swiperCounter = false;
+    }
+  }
+}
+
+let swiperFacilites = false;
+
+
+let swiperCounter = false;
+
+checkVariableSwipers();
+
+window.addEventListener('resize', checkVariableSwipers);
