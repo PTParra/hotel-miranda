a7541d6c7bdbebc75725e68f69d17bf558d1518c
PTParra <49121638+PTParra@users.noreply.github.com>
2024-12-01 18:46:43 +0100

    pagina offers en desktop

--- scripts/offers.js
+++ scripts/offers.js
@@ -1,14 +1,33 @@
-const swiperPopularRooms = new Swiper('.popular-rooms-swiper', {
-    // Optional parameters
-    loop: true,
-    slidesPerView: 1,
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
-});
+let swiperPopularRooms = false;
+
+const checkVariableSwipers = () => {
+  const anchoVentana = window.innerWidth;
+  if (anchoVentana < 1000) {
+    if (!swiperPopularRooms) {
+      swiperPopularRooms = new Swiper('.popular-rooms-swiper', {
+        // Optional parameters
+        loop: true,
+        slidesPerView: 1,
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        autoplay: {
+          delay: 3000,
+          disableOnInteraction: false,
+        },
+    })
+    }
+
+  } else {
+    if (swiperPopularRooms) {
+      swiperPopularRooms.destroy(true, true);
+      swiperPopularRooms = false;
+    }
+  }
+}
+
+checkVariableSwipers();
+
+window.addEventListener('resize', checkVariableSwipers);
